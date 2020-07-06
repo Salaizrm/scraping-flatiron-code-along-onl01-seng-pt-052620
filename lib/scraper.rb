db6e27696b65f4d30962d69a208693057de36d56
Robert Salaiz <work.salaizrm@gmail.com>
2020-07-06 01:14:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,12 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+
+  def get_page
+    html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
+    doc = Nokogiri::HTML(html)
+  end
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,8 +20,5 @@ class Scraper
       end
     end
   end
-  
+
 end
-
-
-
